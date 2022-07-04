7511c1afd5ac37e2e74242297f691aee430b71e1
wolfly22 <wolfly223@126.com>
2022-07-04 22:28:15 +0800

    add psutil.py

--- code/snffier.py
+++ code/snffier.py
@@ -7,24 +7,26 @@ import os
 host = socket.gethostbyname( socket.gethostname())
 
 def main():
+    socket_protocol = None
     if os.name == "nt":
         socket_protocol = socket.IPPROTO_IP
     else:
         socket_protocol = socket.IPPROTO_ICMP
+    
     snffier = socket.socket(socket.AF_INET,socket.SOCK_RAW,socket_protocol)
     snffier.bind((host,0))
     
     #设置数据包头
     snffier.setsockopt(socket.IPPROTO_IP,socket.IP_HDRINCL,1)
-
+    while True:
     #接收数据包
-    if os.name == 'nt':
-        snffier.ioctl(socket.SIO_RCVALL,socket.RCVALL_ON)
-    
-    print(snffier.recvfrom(65565))
+        if os.name == 'nt':
+            snffier.ioctl(socket.SIO_RCVALL,socket.RCVALL_ON)
+        
+        print(snffier.recvfrom(65565))
 
-    if os.name=="nt":
-        snffier.ioctl(socket.SIO_RCVALL,socket.RCVALL_OFF)
+        if os.name=="nt":
+            snffier.ioctl(socket.SIO_RCVALL,socket.RCVALL_OFF)
     
 if __name__ == "__main__":
     main()
